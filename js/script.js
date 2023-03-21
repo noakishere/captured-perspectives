9087fb4d9c34a19e1fbef2dcc4e4d9d1d5509c29
kamyar <kamyarkarimi98@gmail.com>
2023-03-21 15:22:51 -0400

    Image capture and press button implemented

--- js/script.js
+++ js/script.js
@@ -8,19 +8,71 @@ author, and this description to match your project!
 
 "use strict";
 
+let capture;
+let button;
+
+let newPic;
+
 /**
 Description of preload
 */
-function preload() {
-	createCanvas(500, 500);
-}
+function preload() {}
 
 /**
 Description of setup
 */
-function setup() {}
+function setup() {
+	createCanvas(750, 750);
+	capture = createCapture(VIDEO);
+	capture.size(500, 240);
+	capture.hide();
+
+	// button = createButton("snap");
+	// button.mousePressed(takesnap);
+
+	newPic = createImage(500, 240);
+}
 
 /**
 Description of draw()
 */
-function draw() {}
+function draw() {
+	background(255);
+	// image(capture, 0, 0, 500, 240);
+	image(newPic, width / 2, height / 2, 350, 350);
+	// image(capture, 0, 0, 320, 240);
+	filter(INVERT);
+
+	ruler();
+}
+
+function takesnap() {
+	newPic = capture.get(0, 0, 500, 240);
+}
+
+function keyPressed() {
+	if (key == " ") {
+		//this means space bar, since it is a space inside of the single quotes
+		// image(video, 0, 0); //draw the image being captured on webcam onto the canvas at the position (0, 0) of the canvas
+		takesnap();
+	}
+}
+
+/** PROD TOOLS */
+
+/*
+ * have a clearer idea of the points on the canvas
+ * only used for production, won't be used for the final result
+ */
+function ruler() {
+	for (let i = 1; i < 1080; i++) {
+		/* to have a clean look, as without this constraint
+		 * all points would be written on the screen.
+		 */
+		if (i % 50 === 0) {
+			fill("white");
+			text(i, i, 20);
+			text(i, 5, i);
+		}
+	}
+}
